94412473312b2301f3d60e59261043f34415985d
qerobyangnel5-prog <qerobyangnel5@gmail.com>
2026-03-28 22:43:45 +0400

    Create LicenseManager.h

---
+++ LicenseManager.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+#ifndef LICENSEMANAGER_H
+#define LICENSEMANAGER_H
+
+#include <vector>
+#include <string>
+#include "LicenseKey.h"
+
+/**
+ * LicenseManager դաս - լիցենզիաների ստեղծման և ստուգման կառավարում
+ */
+class LicenseManager {
+private:
+    std::vector<LicenseKey> licenses;
+    std::string currentMachineID;
+
+    // Ֆայլից լիցենզիայի վերականգնում
+    bool verifyFromFile(const std::string& filename);
+
+public:
+    LicenseManager();
+
+    // Մշտական լիցենզիայի ստեղծում
+    LicenseKey createPerpetualLicense(const std::string& userName);
+
+    // Ժամանակավոր լիցենզիայի ստեղծում
+    LicenseKey createTemporaryLicense(const std::string& userName, int days);
+
+    // Լիցենզիայի ստուգում
+    bool verifyLicense(const std::string& licenseKey);
+
+    // Լիցենզիայի պահպանում ֆայլում
+    // bool saveToFile(const LicenseKey& license, const std::string& filename);
+
+    // Լիցենզիայի պահպանում ֆայլում (փնտրում է բանալիով)
+    bool saveToFile(const std::string& licenseKey, const std::string& filename);
+
+    // Ցուցադրել բոլոր լիցենզիաները
+    void listAllLicenses();
+
+    // Стуговка оставшихся дней лицензии
+    void checkRemainingDays(const std::string& licenseKey);
+
+    // Ընթացիկ մեքենայի ID-ի ստացում
+    std::string getCurrentMachineID() { return currentMachineID; }
+};
+
+#endif
